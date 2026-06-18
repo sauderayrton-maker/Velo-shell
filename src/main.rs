40fbc2659457346ef2ac99ee4c5c6ab090e391d1
Lost in the Sauce <sauder.ayrton@gmail.com>
2026-06-18 07:26:58 -0600

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use gtk4::gio;
 use gtk4::prelude::*;
 
 mod card;
@@ -6,11 +7,17 @@ mod modules;
 mod window;
 
 fn main() -> glib::ExitCode {
-    let app = gtk4::Application::builder().application_id("com.velo.Shell").build();
+    let app = gtk4::Application::builder()
+        .application_id("com.velo.Shell")
+        .flags(gio::ApplicationFlags::HANDLES_OPEN)
+        .build();
 
     app.connect_activate(|app| {
         window::build_windows(app);
     });
+
+    // Silently ignore any file-open activation attempts.
+    app.connect_open(|_, _, _| {});
 
     app.run()
 }
